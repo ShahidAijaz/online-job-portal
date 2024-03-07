cd09e89001c753c51c4f96ded6f286e07505267d
shahid AIJAZ <aijazshahi67@gmail.com>
2024-03-07 22:44:58 +0530

    first

---
+++ footer.php
@@ -0,0 +1,9 @@
+   <!-- Footer -->
+    <div id="footer">
+        <div id="top" class="noprint"><p><span class="noscreen">Back on top</span> <a href="#header" title="Back on top ^">^<span></span></a></p></div>
+        <hr class="noscreen" />
+        
+        <p class="style1" id="createdby">created by Our taem s2w2<!-- DON´T REMOVE, PLEASE! -->
+        </p>
+        <p>Online-JOB.kmr.com</p>
+</div> <!-- /footer -->
